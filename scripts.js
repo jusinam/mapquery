beebafa84eb3247c38b23b3885eeadf694b6e18d
jusinam <evansonchagwa01@gmail.com>
2022-02-16 21:41:27 +0300

    call function to display selected location on clicking button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-let map;
+let map, input;
 window.onload = function () {
 
     // consuming API KEY
     L.mapquest.key = 'eHBwBAHhVSKwr5veyXHTinizX4GZZyMS';
 
     // reading the select ID
-    // input = document.getElementById('selectLocation');
+    input = document.getElementById('selectLocation');
 
     // 'map' refers to an empty <div> element with the ID map
     map = L.mapquest.map('map', {
@@ -22,5 +22,36 @@ window.onload = function () {
     // L.circle([lat, long], { radius: 2 }).addTo(map)
 
     // map.addLayer(L.mapquest.marketsLayer());
+
+}
+
+function loadIncidentMap() {
+
+    // reading the select ID
+    let selectedValue = document.getElementById("selectLocation").value;
+    L.mapquest.geocoding().geocode(selectedValue);
+
+    // querying the server
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (selectedValue) {
+        if (this.readyState == 4 && this.status == 200) {
+            myJson = JSON.parse(this.responseText);
+
+        }
+    }
+    xhttp.open('GET', 'http://www.mapquestapi.com/traffic/v2/incidents?key=eHBwBAHhVSKwr5veyXHTinizX4GZZyMS&boundingBox=39.95,-105.25,39.52,-104.71&filters=construction,incidents');
+    xhttp.send();
+
+    //  tabular incident rendering            
+    for (let i = 0; i < xhttp.length; i++) {
+
+        let tableBody = document.getElementsById("incidents");
+        let tr = '<tr>';
+
+    tr += '<td><th scope="row">' + 1 + '</th></td>' + '<td' > + xhttp[i].incident + '</td>';
+
+    // adding the table row to the table body 
+    tableBody.innerHTML += tr;
+    }
 
 }
